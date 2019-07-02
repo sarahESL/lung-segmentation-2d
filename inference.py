53b0f3be3e2ddfd2decd6ea17f3d1003a44555e4
Sedigheh Eslami <sedigheh.eslami@gmail.com>
2019-07-02 18:00:40 +0200

    Save performances w.r.t different epsilons
    We store the accuracy in segmentation for different levels of
    perturbation which is defined with epsilon.

--- inference.py
+++ inference.py
@@ -8,6 +8,7 @@ from skimage import morphology, color, io, exposure
 from train_model import train
 import tensorflow as tf
 import keras as K
+import json
 
 def IoU(y_true, y_pred):
     """Returns Intersection over Union score for ground truth and predicted masks."""
@@ -63,8 +64,6 @@ if __name__ == '__main__':
     UNet = load_model(model_name)
     #perturbation = train()
     grad_sign = np.load("/home/sedigheh/lung_segmentation/gradient_signs/grad_wrt_x0.npy")
-    epsilon = 0.2
-    perturbation = grad_sign * epsilon
     # print("******Start of Inference*****")
     # print(perturbation.eval())
     csv_path = '/home/sedigheh/lung_segmentation/dataset/JSRT/preprocessed_org_with_mask/idx.csv'
@@ -77,19 +76,13 @@ if __name__ == '__main__':
     # Load test data
     im_shape = (256, 256)
     df_first = df[df['JPCLN001.png']== 'JPCLN087.png']
-    X, y = loadDataJSRT(df_first, path, im_shape)
-    # X, y = loadDataJSRT(df, path, im_shape)
+    # X, y = loadDataJSRT(df_first, path, im_shape)
+    X, y = loadDataJSRT(df, path, im_shape)
     # print(X[0])
     # print(df_first.shape)
 
     # print("*******Perturb Images******")
 
-    X_adversarial = []
-    for i in range(X.shape[0]):
-        X_adversarial.append(X[i] + perturbation[i])
-        # X_adversarial.append(X[i] + np.random.normal(0, 1, X[0].shape))
-
-    X_adversarial = np.array(X_adversarial)
     # print("Original X shape: ", X.shape)
     # print("Adversarial X shape: ", X_adversarial.shape)
 
@@ -106,9 +99,8 @@ if __name__ == '__main__':
 
     ious = np.zeros(n_test)
     dices = np.zeros(n_test)
-
     i = 0
-    """
+
     for xx, yy in test_gen.flow(X, y, batch_size=1):
         img = exposure.rescale_intensity(np.squeeze(xx), out_range=(0,1))
         pred = UNet.predict(xx)[..., 0].reshape(inp_shape[:2])
@@ -121,10 +113,9 @@ if __name__ == '__main__':
         # Remove regions smaller than 2% of the image
         pr = remove_small_regions(pr, 0.02 * np.prod(im_shape))
 
-        # io.imsave('results-ct/{}'.format(df.iloc[i][0]), masked(img, gt, pr, 1))
+        io.imsave('results/{}'.format(df.iloc[i][0]), masked(img, gt, pr, 1))
         print(img)
-        io.imsave('results-ct/{}'.format(df.iloc[i][0]), img)
-        input("wait")
+        # io.imsave('results/{}'.format(df.iloc[i][0]), img)
 
         ious[i] = IoU(gt, pr)
         dices[i] = Dice(gt, pr)
@@ -133,30 +124,51 @@ if __name__ == '__main__':
         i += 1
         if i == n_test:
             break
+    print('Mean IoU:', ious.mean())
+    print('Mean Dice:', dices.mean())
 
     """
     print("******Evaluation with adversarial examples*******")
-    for xx, yy in test_gen.flow(X_adversarial, y, batch_size=1):
-        img = exposure.rescale_intensity(np.squeeze(xx), out_range=(0,1))
-        pred = UNet.predict(xx)[..., 0].reshape(inp_shape[:2])
-        mask = yy[..., 0].reshape(inp_shape[:2])
+    epsilon = [0, 0.01, 0.05, 0.1, 0.15, 0.2, 0.3]
+    results = {}
+    for eps in epsilon:
+        perturbation = grad_sign * eps
+        X_adversarial = []
+        for i in range(X.shape[0]):
+            X_adversarial.append(X[i] + perturbation[i])
+            # X_adversarial.append(X[i] + np.random.normal(0, 1, X[0].shape))
+
+        X_adversarial = np.array(X_adversarial)
+        ious = np.zeros(n_test)
+        dices = np.zeros(n_test)
+        i = 0
+        for xx, yy in test_gen.flow(X_adversarial, y, batch_size=1):
+            img = exposure.rescale_intensity(np.squeeze(xx), out_range=(0,1))
+            pred = UNet.predict(xx)[..., 0].reshape(inp_shape[:2])
+            mask = yy[..., 0].reshape(inp_shape[:2])
 
         # Binarize masks
-        gt = mask > 0.5
-        pr = pred > 0.5
+            gt = mask > 0.5
+            pr = pred > 0.5
 
         # Remove regions smaller than 2% of the image
-        pr = remove_small_regions(pr, 0.02 * np.prod(im_shape))
+            pr = remove_small_regions(pr, 0.02 * np.prod(im_shape))
 
-        io.imsave('results_adversarial_examples/{}'.format(df_first.iloc[i][0]), img)
-        io.imsave('results_adversarial_segmentations/{}'.format(df_first.iloc[i][0]), masked(img, gt, pr, 1))
+            io.imsave('results_adversarial_examples/{}_{}'.format(eps, df_first.iloc[i][0]), img)
+            io.imsave('results_adversarial_segmentations/{}_{}'.format(eps, df_first.iloc[i][0]), masked(img, gt, pr, 1))
 
-        ious[i] = IoU(gt, pr)
-        dices[i] = Dice(gt, pr)
-        print(df_first.iloc[i][0], ious[i], dices[i])
+            ious[i] = IoU(gt, pr)
+            dices[i] = Dice(gt, pr)
+            print(df_first.iloc[i][0], ious[i], dices[i])
+            performance_file_name = df_first.iloc[i][0]
 
-        i += 1
-        if i == n_test:
-            break
-    print('Mean IoU:', ious.mean())
-    print('Mean Dice:', dices.mean())
+            i += 1
+            if i == n_test:
+                break
+        print('Mean IoU:', ious.mean())
+        print('Mean Dice:', dices.mean())
+        results[eps] = {"ious_mean": ious.mean(), "dices_mean": dices.mean()}
+
+    with open("performances/fgsm_jsrt.json", 'w') as f:
+        json.dump(results, f)
+    """
